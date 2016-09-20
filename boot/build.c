92622e29f830dd25e2a88ac6f3b094e3417e4449
NEWPLAN <enwplan001@163.com>
2016-09-20 08:58:57 +0800

    mod format

--- boot/build.c
+++ boot/build.c
@@ -22,53 +22,59 @@
 
 void copy_file(FILE *img, FILE *sys)
 {
-	char buf[512];
-	for (int i = 1; i < 80; i++) {
-		fseek(img, 18 * 2 * 512 * i, 0);
-		for (int j = 0; j < 18; j++) {
-			if (fread(buf, 1, 512, sys))
-				fwrite(buf, 1, 512, img);
-			else
-				return;
-		}
-	}
+    char buf[512];
+    for (int i = 1; i < 80; i++)
+    {
+        fseek(img, 18 * 2 * 512 * i, 0);
+        for (int j = 0; j < 18; j++)
+        {
+            if (fread(buf, 1, 512, sys))
+                fwrite(buf, 1, 512, img);
+            else
+                return;
+        }
+    }
 }
 
 int main(int argc, char **argv)
 {
-	FILE *boot, *sys, *img;
-	char buf[512];
+    FILE *boot, *sys, *img;
+    char buf[512];
 
-	if (argc != 4) {
-		fprintf(stderr, "Usage: build boot system image\n");
-		exit(0);
-	}
+    if (argc != 4)
+    {
+        fprintf(stderr, "Usage: build boot system image\n");
+        exit(0);
+    }
 
-	boot = fopen(argv[1], "rb");
-	if (boot == NULL) {
-		fprintf(stderr, "File Open Error:%s", argv[1]);
-		exit(0);
-	}
+    boot = fopen(argv[1], "rb");
+    if (boot == NULL)
+    {
+        fprintf(stderr, "File Open Error:%s", argv[1]);
+        exit(0);
+    }
 
-	sys = fopen(argv[2], "rb");
-	if (sys == NULL) {
-		fprintf(stderr, "File Open Error:%s", argv[2]);
-		exit(0);
-	}
+    sys = fopen(argv[2], "rb");
+    if (sys == NULL)
+    {
+        fprintf(stderr, "File Open Error:%s", argv[2]);
+        exit(0);
+    }
 
-	img = fopen(argv[3], "wb");
-	if (img == NULL) {
-		fprintf(stderr, "File Open Error:%s", argv[3]);
-		exit(0);
-	}
+    img = fopen(argv[3], "wb");
+    if (img == NULL)
+    {
+        fprintf(stderr, "File Open Error:%s", argv[3]);
+        exit(0);
+    }
 
-	fread(buf, 512, 1, boot);
-	fwrite(buf, 512, 1, img);
+    fread(buf, 512, 1, boot);
+    fwrite(buf, 512, 1, img);
 
-	copy_file(img, sys);
+    copy_file(img, sys);
 
-	fseek(img, SIZE - 1, 0);
-	fputc(0, img);
+    fseek(img, SIZE - 1, 0);
+    fputc(0, img);
 
-	fclose(img);
+    fclose(img);
 }
