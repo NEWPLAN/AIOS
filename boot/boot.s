f8ab037b6274e5320dd0578aa4f155fbf08a1ecd
NEWPLAN <enwplan001@163.com>
2016-09-21 16:56:15 +0800

    make notes

--- boot/boot.s
+++ boot/boot.s
@@ -14,9 +14,41 @@
 ; *  See the License for the specific language governing permissions and
 ; *  limitations under the License.
 ; */
+;
+;1:数据寄存器,一般称之为通用寄存器组     8086 有8个8位数据寄存器，
+;	这些8位寄存器可分别组成16位寄存器：
+;	AH&AL＝AX：累加寄存器，常用于运算；
+;	BH&BL＝BX：基址寄存器，常用于地址索引；
+;	CH&CL＝CX：计数寄存器，常用于计数；
+;	DH&DL＝DX：数据寄存器，常用于数据传递。   
+;
+;2:地址寄存器/段地址寄存器     为了运用所有的内存空间，8086设定了四个段寄存器，专门用来保存段地址：
+;	CS（Code Segment）：代码段寄存器；
+;	DS（Data Segment）：数据段寄存器；
+;	SS（Stack Segment）：堆栈段寄存器；
+;	ES（Extra Segment）：附加段寄存器。
+;
+;3：特殊功能的寄存器
+;	IP（Instruction Pointer）：指令指针寄存器，与CS配合使用，可跟踪程序的执行过程；
+;	SP（Stack Pointer）：堆栈指针，与SS配合使用，可指向目前的堆栈位置。
+;	BP（Base Pointer）：基址指针寄存器，可用作SS的一个相对基址位置；
+;	SI（Source Index）：源变址寄存器可用来存放相对于DS段之源变址指针；
+;	DI（Destination Index）：目的变址寄存器，可用来存放相对于 ES 段之目的变址指针。
+;
+;4：标志寄存器：保存CPU的运行状态
+;	OF overflow flag 溢出标志 操作数超出机器能表示的范围表示溢出,溢出时为1.
+;	SF sign Flag 符号标志 记录运算结果的符号,结果负时为1.
+;	ZF zero flag 零标志 运算结果等于0时为1,否则为0.
+;	CF carry flag 进位标志 最高有效位产生进位时为1,否则为0.
+;	AF auxiliary carry flag 辅助进位标志 运算时,第3位向第4位产生进位时为1,否则为0.
+;	PF parity flag 奇偶标志 运算结果操作数位为1的个数为偶数个时为1,否则为0.
+;	DF direcion flag 方向标志 用于串处理.DF=1时,每次操作后使SI和DI减小.DF=0时则增大.
+;	IF interrupt flag 中断标志 IF=1时,允许CPU响应可屏蔽中断,否则关闭中断.
+;	TF trap flag 陷阱标志 用于调试单步操作.
+;
 
-	bits 16
-start:
+	bits 16 ;十六位，实模式下
+start:;拷贝信息，sx:si->ds:di
 	mov ax,0x07c0
 	mov ds,ax
 	mov ax,0x800
